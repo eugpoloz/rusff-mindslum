3172b16e09d628c7724a53e19a6ac25561abb7da
Evgenia Polozova <eugpoloz@gmail.com>
2017-06-17 01:52:36 +0300

    refactor script a bit

--- removeFldNames.js
+++ removeFldNames.js
@@ -1,10 +1,12 @@
-(function removeFldNames() {
-	let fldList = document.querySelectorAll('.post-author li[class^="pa-fld"]');
+(function removeFldNames(flds) {
+  let queryClasses = [];
+  flds.forEach(fld => queryClasses.push(`.post-author li.pa-fld${fld}`));
 
-	fldList.forEach(node => {
-		let arrHTML = node.innerHTML.split(": ");
-		arrHTML.splice(0, 1);
+  let fldList = document.querySelectorAll(queryClasses.join(', '));
+  fldList.forEach(node => {
+    let arrHTML = node.innerHTML.split(": ");
+    arrHTML.splice(0, 1);
 
-		return node.innerHTML = arrHTML.join("");
-	});
-})();
+    return node.innerHTML = arrHTML.join("");
+  });
+})([1]);
